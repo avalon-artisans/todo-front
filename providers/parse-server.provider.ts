e5d4703c04304655dbf340d739090ecf50d7d1ed
Kenneth Sumang <kennethsumang08@gmail.com>
2023-08-20 21:54:03 +0800

    chore: add parse server url in provider

--- providers/parse-server.provider.ts
+++ providers/parse-server.provider.ts
@@ -1,9 +1,11 @@
 interface ParseServerConfig {
+  serverUrl: string;
   applicationId: string;
   restApiKey: string;
 }
 
 export const parseServerConfig: ParseServerConfig = {
+  serverUrl: process.env.PARSE_SERVER_URL as string,
   applicationId: process.env.PARSE_APPLICATION_ID as string,
   restApiKey: process.env.PARSE_REST_API_KEY as string,
 };
