650c288fec41863dbd03d78433478d7f53dc8e23
Kenneth Sumang <kennethsumang08@gmail.com>
2023-09-02 22:30:36 +0800

    feat: change upcoming filter logic

--- filters/todo/upcoming-todos.filter.ts
+++ filters/todo/upcoming-todos.filter.ts
@@ -2,6 +2,9 @@ import {TodoItem, TodoStatus} from '@/types/todo';
 import BaseFilter from '@/filters/base.filter';
 import _ from 'lodash';
 import dayjs from 'dayjs';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+
+dayjs.extend(isSameOrAfter);
 
 export default class UpcomingTodosFilter<T extends TodoItem> extends BaseFilter<T> {
   applyFilter(items: T[], options?: Record<string, any>): T[] {
@@ -11,9 +14,9 @@ export default class UpcomingTodosFilter<T extends TodoItem> extends BaseFilter<
       }
 
       const dueDate = dayjs(item.due_date, 'YYYY-MM-DD HH:mm:ss');
-      const timeNow = dayjs();
+      const tomorrow = dayjs().add(1, 'day').startOf('day');
 
-      return timeNow.isBefore(dueDate) && item.status !== TodoStatus.DONE;
+      return dueDate.isSameOrAfter(tomorrow) && item.status !== TodoStatus.DONE;
     });
   }
 }
